a580176ff01848725ebf43ced62e43ac4da67dff
Ryan Bowman <ryan@circle.co.ke>
2018-07-22 16:29:44 +0100

    front page

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/main.css': 'css/main.sass'
+                }
+            }
+        },
+
+
+
+        watch: {
+
+            css: {
+                files: ['css/*', '/*'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            },
+            options: {
+                atBegin: true,
+                livereload: true
+            }
+        },
+
+
+
+
+    });
+
+    grunt.registerTask('build', ['sass']);
+    grunt.registerTask('default', ['watch']);
+
+};
